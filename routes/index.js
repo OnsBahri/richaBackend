b0a3652be1e9e7b66a66cb5d1330cafc934ec2cb
OnsBahri <73686871+OnsBahri@users.noreply.github.com>
2021-06-12 14:18:30 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const auth = require('./auth.route');
 const category = require('./category.route');
+const order = require('./order.route');
+const product = require('./product.router');
+
 
 router.use('/user/', auth);
+router.use('/product/', product);
+router.use('/order/',order);
 // router.use('/category/', category);
 
 module.exports = router;
